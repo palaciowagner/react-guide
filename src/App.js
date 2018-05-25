d2043a06478fd8da17b514afa731dc2d81a3cde7
Wagner Palacio <palaciowagner@gmail.com>
2018-05-25 18:18:50 -0300

    added switchNameHandler to have a state inside App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Wagner", age: 24},
+      {name: "Beto", age: 1},
+      {name: "Jean", age: 27}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('This was clicked indeed!');
+    this.setState({
+      persons: [
+        {name: "Vanjie", age: 24},
+        {name: "Beto", age: 30},
+        {name: "Jean", age: 27}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Wagner" age="24"/>
-        <Person name="Beto" age="1">My hobby: meowing</Person>
-        <Person name="Jean" age="27"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
